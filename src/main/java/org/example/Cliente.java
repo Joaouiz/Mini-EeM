db789a9883e9ee49008efb54d6526224e32bcd7f
Joaouiz <joaoluizordines@hotmail.com>
2026-06-24 23:25:51 -0300

    dua

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    private final int id;
+    private final String nome, status;
+    private final double valor;
+    private final LocalDate vencimento;
+    private final String telefone;
+
+    public Cliente (int id, String nome, String telefone, double valor, LocalDate vencimento, String status){
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.valor = valor;
+        this.vencimento = vencimento;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mariadb://localhost:3306/automacao";
+
+        String usuario = "root";
+        String senha = "9998";
+
+        List<Cliente> listaClientes = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url, usuario, senha)) {
+            System.out.println("Conectado!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM clientes");
+
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String telefone = rs.getString("telefone");
+                double valor = rs.getDouble("valor");
+                LocalDate vencimento = rs.getDate("vencimento").toLocalDate();
+                /*
+                Você pode usar o rs.getObject() passando a classe desejada como segundo parâmetro. Fica super limpo:
+                // Busca direto do ResultSet já convertendo para LocalDate
+                LocalDate data = rs.getObject("nome_da_coluna", LocalDate.class);
+                 */
+                String status = rs.getString("status");
+                Cliente kpop = new Cliente(id, nome, telefone, valor, vencimento, status);
+                listaClientes.add(kpop);
+            }
+
+            for(Cliente e : listaClientes){
+                System.out.println("ID: " + e.getId() + " | Nome: " + e.getNome() + " | Vencimento: " + e.getVencimento() + " | Status: " + e.getStatus());
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.writeValue(new File("clientes.json"), listaClientes);
+
+            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(listaClientes);
+
+            System.out.println(json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
         }
     }
 }
